385467862068ef1c4c598815470eabfd3f54f6a1
pppp2254 <punalmonsalmon@gmail.com>
2026-01-23 15:27:56 +0700

    backedn end

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.orm import Mapped, mapped_column
-from flask_migrate import Migrate  
-from sqlalchemy import Integer, String, ForeignKey                           
-from sqlalchemy.orm import Mapped, mapped_column, relationship                
+from flask_migrate import Migrate
+
+from models import TodoItem, Comment, db                             
 
 app = Flask(__name__)
-
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-CORS(app)
+
+db.init_app(app)                                                     # แก้จาก db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db)            
+
 
 todo_list = [
     { "id": 1,
@@ -24,44 +22,6 @@ todo_list = [
       "done": False },
 ]
 
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(app, model_class=Base)
-migrate = Migrate(app, db)    
-
-
-class TodoItem(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(100))
-    done: Mapped[bool] = mapped_column(default=False)
-
-    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
-    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "done": self.done,
-            "comments": [
-                comment.to_dict() for comment in self.comments
-            ]
-        }
-
-class Comment(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    message: Mapped[str] = mapped_column(String(250))
-    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
-
-    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "message": self.message,
-            "todo_id": self.todo_id
-        }
 
 
 INITIAL_TODOS = [
