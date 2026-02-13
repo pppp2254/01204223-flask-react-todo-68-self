9be399a05075eac650dd1da78dc9e778a26dde64
pppp2254 <punalmonsalmon@gmail.com>
2026-02-13 14:48:15 +0700

    fixed_comment

--- frontend/src/TodoItem.jsx
+++ frontend/src/TodoItem.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
+import './App.css'
 
 function TodoItem({ todo, toggleDone, deleteTodo, addNewComment }) {
   const [newComment, setNewComment] = useState("");
 
+  const commentCount = todo.comments ? todo.comments.length : 0;
+
   return (
     <li>
-      <span>{todo.title}</span>
+      <span className={todo.done ? "done" : ""}>{todo.title}</span>
       <button onClick={() => toggleDone?.(todo.id)}>Toggle</button>
       <button onClick={() => deleteTodo?.(todo.id)}>❌</button>
 
-      {todo.comments && todo.comments.length > 0 ? (
-        <ul>
-          {todo.comments.map(comment => (
-            <li key={comment.id}>{comment.message}</li>
-          ))}
-        </ul>
+      {commentCount > 0 ? (
+        <>
+          <br /><b>Comments ({commentCount}):</b>
+          <ul>
+            {todo.comments.map(comment => (
+              <li key={comment.id}>{comment.message}</li>
+            ))}
+          </ul>
+        </>
       ) : (
         <p>No comments</p>
       )}
@@ -33,7 +39,7 @@ function TodoItem({ todo, toggleDone, deleteTodo, addNewComment }) {
         </button>
       </div>
     </li>
-  );
+  )
 }
 
-export default TodoItem; // ตรวจสอบบรรทัดนี้ให้ดีครับ!
+export default TodoItem
