143372484c81346d9d90ee3daf86320706817672
pppp2254 <punalmonsalmon@gmail.com>
2026-02-27 12:37:38 +0700

    add pytest_path

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,4 +1,5 @@
 from models import User
+from models import TodoItem, Comment, db
 
 def test_check_correct_password():
     user = User()
@@ -9,3 +10,6 @@ def test_check_incorrect_password():
     user = User()
     user.set_password("testpassword")
     assert user.check_password("testpassworx") == False
+
+def test_empty_todoitem(app_context):
+    assert TodoItem.query.count() == 0
